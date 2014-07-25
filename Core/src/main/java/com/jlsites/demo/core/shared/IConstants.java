8638344d4047f0125c797aef2d587c8a63ffa712
jlSites <jlsites.git@gmail.com>
2014-07-25 23:10:15 +1000

    provided a customized upload servlet to save the files into another public 'uploads' web app, which just contains public uploaded files

--- Core/src/main/java/com/jlsites/demo/core/shared/IConstants.java
+++ Core/src/main/java/com/jlsites/demo/core/shared/IConstants.java
@@ -3,5 +3,8 @@ package com.jlsites.demo.core.shared;
 public interface IConstants {
 	String ENV_CATALINA_BASE = "CATALINA_BASE";
 
+	/**
+	 * another public web app, which just contains public-accessable files
+	 */
 	String UPLOADS_FOLDER = "/webapps/uploads/";
 }
